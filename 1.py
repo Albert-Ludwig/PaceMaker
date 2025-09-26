a4a1e4c6d6d464daedc5c569612118861cf87261
ke1b <kelbyto@gmail.com>
2025-09-26 15:57:22 -0400

    last test?

--- 1.py
+++ 1.py
@@ -1,19 +1,4 @@
 import time
 import sys
 
-<<<<<<< HEAD
-fake_messages = [
-    "Encrypting all files... 10%",
-    "Encrypting all files... 25%",
-    "Encrypting all files... 50%",
-    "Encrypting all files... 75%",
-    "Encrypting all files... 99%",
-    "Error: Cannot stop process...",
-    "Just kidding! This is a joke :)"
-]
-
-for msg in fake_messages:
-    print(msg)
-    time.sleep(1.5)
-
-sys.exit(0)
+print("hello world")
