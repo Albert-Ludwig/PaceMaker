b1190e8e95775c668dfdaee953ec4987e85b3df4
Arian <ariannosratinik@gmail.com>
2025-09-26 15:53:09 -0400

    not malware

--- 1.py
+++ 1.py
@@ -1,3 +1,18 @@
-print("Hello, World!")
+import time
+import sys
 
-# testing 123 simulink Ryan
+fake_messages = [
+    "Encrypting all files... 10%",
+    "Encrypting all files... 25%",
+    "Encrypting all files... 50%",
+    "Encrypting all files... 75%",
+    "Encrypting all files... 99%",
+    "Error: Cannot stop process...",
+    "Just kidding! This is a joke :)"
+]
+
+for msg in fake_messages:
+    print(msg)
+    time.sleep(1.5)
+
+sys.exit(0)
