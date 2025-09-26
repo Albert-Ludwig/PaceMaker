56678f86ee00a3b3f75a81243faa858c90823e97
Johnson Haoran Ji <johnsonji0602@gmail.com>
2025-09-26 16:00:50 -0400

    9/26

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 import os
+class ApplicationWindow:
+    def __init__(self,root, username):
+        self.root=root
+        self.root.title("Pacing Mode Selection")
+        self.root.geometry("1920x1080")
+
+        self.username=username
+        
